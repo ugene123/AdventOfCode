fd7ada9932cdfddcb4216a9da5b3d96260894d7c
ugene123 <unconfigured@null.spigotmc.org>
2020-12-02 15:46:03 -0500

    refactor limits to min/max

--- 2020/day2/day2.py
+++ 2020/day2/day2.py
@@ -4,23 +4,24 @@ def getComponents(entry):
   limits = [int(x) for x in entry[0].split("-")]
   char = entry[1].strip(":")
   password = entry[2]
-  return limits, char, password
+  return limits[0], limits[1], char, password
 
 def part1(entries):
   valid = 0
   for entry in entries:
-    limits, char, password = getComponents(entry)
+    lmin, lmax, char, password = getComponents(entry)
     count = password.count(char)
-    if count >= limits[0] and count <= limits[1]:
+    if count >= lmin and count <= lmax:
       valid += 1
   return valid
 
 def part2(entries):
   valid = 0
   for entry in entries:
-    limits, char, password = getComponents(entry)
-    pos = [int(x)-1 for x in limits]
-    if (password[pos[0]] == char and password[pos[1]] != char) or (password[pos[0]] != char and password[pos[1]] == char):
+    lmin, lmax, char, password = getComponents(entry)
+    pos1 = lmin - 1
+    pos2 = lmax - 1
+    if (password[pos1] == char and password[pos2] != char) or (password[pos1] != char and password[pos2] == char):
       valid += 1
   return valid
 
